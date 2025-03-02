6d25ca1bcaf95d157666e8d853559d361537c124
vijinak <vijinavijayankv@gmail.com>
2025-03-02 11:19:09 +0530

    'final'

--- src/service/serverUrl.js
+++ src/service/serverUrl.js
@@ -1,3 +1,6 @@
-export const serverUrl= 'http://localhost:4010'
+// export const serverUrl= 'http://localhost:4010'
 
 // export const serverUrl = "https://projectfair-server-1-bh9m.onrender.com"
+
+// my
+export const serverUrl =  "https://project-fair-server-xxz8.onrender.com"
